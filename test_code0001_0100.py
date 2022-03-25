cb9f6b48da3dc66192273d52a54f58f983d357d2
Aidan <hyideal@gmail.com>
2022-03-25 23:33:31 +0800

    调整测试结构

--- test_code0001_0100.py
+++ test_code0001_0100.py
@@ -2,9 +2,24 @@ from typing import List
 
 
 # 两数之和
+# 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+# 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
 def test0001():
     # Eric 2022-03-25
     def two_sum(nums, target):
+        return aidan220325(nums, target)
+        # return eric220325(nums, target)
+
+    def aidan220325(nums, target):
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return []
+
+    def eric220325(nums, target):
         index = 0
         # 依次选取数组中的数子，并依次与所有后面的数字相加，判断和是否为8
         while index < len(nums) - 1:
@@ -75,6 +90,9 @@ def test0002():
 # 给定一个字符串s ，请你找出其中不含有重复字符的最长子串的长度。
 def test0003():
     def length_of_longest_substring(s):
+        return eric220326(s)
+
+    def eric220326(s):
         pass
 
     s = "abcabcbb"
--- test_code0101_0200.py
+++ test_code0101_0200.py
@@ -2,12 +2,22 @@ from typing import List
 
 
 # 买卖股票的最佳时机 II
+# 给定一个数组 prices ，其中prices[i] 表示股票第 i 天的价格。
+# 在每一天，你可能会决定购买和/或出售股票。你在任何时候最多只能持有 一股 股票。你也可以购买它，然后在 同一天 出售。
+# 返回 你能获得的 最大 利润。
+
 def test0122():
-    # Aidan
+
+    def max_profit(prices):
+        return eric220324(prices)
+        # return aidan220324(prices)
+        # return leecode(prices)
+
+
     # 买入点：没遇到涨，买入点就前移，直到最后一天
     # 卖出点：买入过，遇到跌，前一天肯定是卖出点，交易
     # 边界：最后一天是涨的话，第二条件不满足，（只确认了买入点，还没遇到跌的情况），交易
-    def max_profit(prices: List[int]) -> int:
+    def aidan220324(prices: List[int]) -> int:
         if not prices:
             return 0
 
@@ -30,14 +40,13 @@ def test0122():
 
         return profit
 
-    # Eric
     # 分析折线图
     # 买入点：一个点左边平或者高，右边高，就是买入点，记录
     # 卖出点：一个点左边平或者低，右边低，就是买出点，记录
     # 边界：判断第一天是否买入点，判断最后一天是否卖出点
     #      卖出点前必定存在买入点
     # 结算：依次交易，返回累积收益
-    def max_profit1(prices):
+    def eric220324(prices):
 
         buy = []
         sell = []
@@ -66,7 +75,7 @@ def test0122():
 
     # 官方算法
     # 贪心法，遇到涨就结算当地的收益，返回累计收益
-    def max_profit0(prices: List[int]) -> int:
+    def leecode(prices: List[int]) -> int:
         if not prices:
             return 0
 
@@ -80,22 +89,22 @@ def test0122():
         return profit
 
     prices = [7, 1, 5, 3, 6, 4]
-    assert max_profit1(prices) == 7
+    assert max_profit(prices) == 7
 
     prices = [1, 2, 3, 4, 5]
-    assert max_profit1(prices) == 4
+    assert max_profit(prices) == 4
 
     prices = [2, 2, 5]
-    assert max_profit1(prices) == 3
+    assert max_profit(prices) == 3
 
     prices = [1, 1, 0]
-    assert max_profit1(prices) == 0
+    assert max_profit(prices) == 0
 
     prices = [8, 6, 4, 3, 3, 2, 3, 5, 8, 3, 8, 2, 6]
-    assert max_profit1(prices) == 15
+    assert max_profit(prices) == 15
 
     prices = []
-    assert max_profit1(prices) == 0
+    assert max_profit(prices) == 0
 
     prices = [1]
-    assert max_profit1(prices) == 0
+    assert max_profit(prices) == 0
