0a936f984ab90d3d8014054b20b2f16040b15365
Aidan <hyideal@gmail.com>
2022-03-30 21:30:49 +0800

    added problem 0011

--- test_code0001_0100.py
+++ test_code0001_0100.py
@@ -226,3 +226,25 @@ def test0009():
     assert is_palindrome(x) is False
     x = 10
     assert is_palindrome(x) is False
+
+
+# 盛最多水的容器
+
+# 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+# 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+# 返回容器可以储存的最大水量。
+
+# 说明：你不能倾斜容器。
+def test0011():
+    def max_area(height):
+        return eric220331()
+
+    def eric220331(height):
+        pass
+
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    assert max_area(height) == 49
+
+    height = [1, 1]
+    assert max_area(height) == 1
+
